c13c0a373109a6a32fb5afb98589d64860ed58e4
Lionel <472714747@qq.com>
2019-12-04 23:02:31 +0800

    1

---
+++ 单例模式多线程.py
@@ -0,0 +1,27 @@
+import threading, time
+
+
+class Singleton(object):
+    _instance_lock = threading.Lock()
+
+    def __init__(self):
+        pass
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(Singleton, "_instance"):
+            with Singleton._instance_lock:
+                if not hasattr(Singleton, "_instance"):
+                    Singleton._instance = object.__new__(cls)
+        return Singleton._instance
+
+
+def task(arg):
+    obj = Singleton()
+    print(obj)
+
+
+for i in range(10):
+    t = threading.Thread(target=task, args=[
+        i,
+    ])
+    t.start()
