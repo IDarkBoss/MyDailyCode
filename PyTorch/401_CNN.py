4b20fbfda216086a814337447c131484a7632bf2
Lionel <472714747@qq.com>
2020-01-08 16:54:49 +0800

    1

--- PyTorch/401_CNN.py
+++ PyTorch/401_CNN.py
@@ -20,44 +20,6 @@ import matplotlib.pyplot as plt
 
 # torch.manual_seed(1)    # reproducible
 
-# Hyper Parameters
-EPOCH = 1  # train the training data n times, to save time, we just train 1 epoch
-BATCH_SIZE = 50
-LR = 0.001  # learning rate
-DOWNLOAD_MNIST = False
-
-# Mnist digits dataset
-if not (os.path.exists('./mnist/')) or not os.listdir('./mnist/'):
-    # not mnist dir or mnist is empyt dir
-    DOWNLOAD_MNIST = True
-
-train_data = torchvision.datasets.MNIST(
-    root="./PyTorch/mnist/",
-    train=True,  # this is training data
-    transform=torchvision.transforms.ToTensor(),
-    # Converts a PIL.Image or numpy.ndarray to torch.FloatTensor of shape (C x H x W) and normalize in the range [0.0, 1.0]
-    download=DOWNLOAD_MNIST,
-)
-
-# plot one example
-print(train_data.data.size())  # (60000, 28, 28)
-print(train_data.targets.size())  # (60000)
-plt.imshow(train_data.data[0].numpy(), cmap='gray')
-plt.title('%i' % train_data.targets[0])
-plt.show()
-
-# Data Loader for easy mini-batch return in training, the image batch shape will be (50, 1, 28, 28)
-train_loader = Data.DataLoader(dataset=train_data,
-                               batch_size=BATCH_SIZE,
-                               shuffle=True)
-
-# pick 2000 samples to speed up testing
-test_data = torchvision.datasets.MNIST(root="./PyTorch/mnist/", train=False)
-test_x = torch.unsqueeze(test_data.data, dim=1).type(
-    torch.FloatTensor)[:2000] / 255.
-# shape from (2000, 28, 28) to (2000, 1, 28, 28), value in range(0,1)
-test_y = test_data.targets[:2000]
-
 
 class CNN(nn.Module):
     def __init__(self):
@@ -94,23 +56,6 @@ class CNN(nn.Module):
         return output, x  # return x for visualization
 
 
-cnn = CNN()
-print(cnn)  # net architecture
-
-optimizer = torch.optim.Adam(cnn.parameters(),
-                             lr=LR)  # optimize all cnn parameters
-loss_func = nn.CrossEntropyLoss()  # the target label is not one-hotted
-
-# following function (plot_with_labels) is for visualization, can be ignored if not interested
-from matplotlib import cm
-try:
-    from sklearn.manifold import TSNE
-    HAS_SK = True
-except:
-    HAS_SK = False
-    print('Please install sklearn for layer visualization')
-
-
 def plot_with_labels(lowDWeights, labels):
     plt.cla()
     X, Y = lowDWeights[:, 0], lowDWeights[:, 1]
@@ -124,43 +69,104 @@ def plot_with_labels(lowDWeights, labels):
     plt.pause(0.01)
 
 
-plt.ion()
-# training and testing
-for epoch in range(EPOCH):
-    for step, (b_x, b_y) in enumerate(
-            train_loader
-    ):  # gives batch data, normalize x when iterate train_loader
+if __name__ == "__main__":
+    torch.multiprocessing.set_start_method("spawn")
 
-        output = cnn(b_x)[0]  # cnn output
-        loss = loss_func(output, b_y)  # cross entropy loss
-        optimizer.zero_grad()  # clear gradients for this training step
-        loss.backward()  # backpropagation, compute gradients
-        optimizer.step()  # apply gradients
+    # Hyper Parameters
+    EPOCH = 1  # train the training data n times, to save time, we just train 1 epoch
+    BATCH_SIZE = 500
+    LR = 0.001  # learning rate
+    DOWNLOAD_MNIST = False
 
-        if step % 50 == 0:
-            test_output, last_layer = cnn(test_x)
-            pred_y = torch.max(test_output, 1)[1].detach().numpy()
-            accuracy = float(
-                (pred_y == test_y.detach().numpy()).astype(int).sum()) / float(
-                    test_y.size(0))
-            print('Epoch: ', epoch,
-                  '| train loss: %.4f' % loss.detach().numpy(),
-                  '| test accuracy: %.2f' % accuracy)
-            if HAS_SK:
-                # Visualization of trained flatten layer (T-SNE)
-                tsne = TSNE(perplexity=30,
-                            n_components=2,
-                            init='pca',
-                            n_iter=5000)
-                plot_only = 500
-                low_dim_embs = tsne.fit_transform(
-                    last_layer.detach().numpy()[:plot_only, :])
-                labels = test_y.numpy()[:plot_only]
-                # plot_with_labels(low_dim_embs, labels)
-plt.ioff()
+    # Mnist digits dataset
+    if not (os.path.exists('./PyTorch/mnist/')
+            ) or not os.listdir('./PyTorch/mnist/'):
+        # not mnist dir or mnist is empyt dir
+        DOWNLOAD_MNIST = True
 
-# print 10 predictions from test data
-test_output, _ = cnn(test_x[:10])
-pred_y = torch.max(test_output, 1)[1].detach().numpy()
-print(pred_y, 'prediction number')
-print(test_y[:10].numpy(), 'real number')
+    train_data = torchvision.datasets.MNIST(
+        root="./PyTorch/mnist/",
+        train=True,  # this is training data
+        transform=torchvision.transforms.ToTensor(),
+        # Converts a PIL.Image or numpy.ndarray to torch.FloatTensor of shape (C x H x W) and normalize in the range [0.0, 1.0]
+        download=DOWNLOAD_MNIST,
+    )
+
+    # plot one example
+    print(train_data.data.size())  # (60000, 28, 28)
+    print(train_data.targets.size())  # (60000)
+    '''
+    plt.imshow(train_data.data[0].numpy(), cmap='gray')
+    plt.title('%i' % train_data.targets[0])
+    plt.show()
+    '''
+
+    # Data Loader for easy mini-batch return in training, the image batch shape will be (50, 1, 28, 28)
+    train_loader = Data.DataLoader(dataset=train_data,
+                                   batch_size=BATCH_SIZE,
+                                   shuffle=True,
+                                   num_workers=2)
+
+    # pick 2000 samples to speed up testing
+    test_data = torchvision.datasets.MNIST(root="./PyTorch/mnist/",
+                                           train=False)
+    test_x = torch.unsqueeze(test_data.data, dim=1).type(
+        torch.FloatTensor)[:2000] / 255.
+    # shape from (2000, 28, 28) to (2000, 1, 28, 28), value in range(0,1)
+    test_y = test_data.targets[:2000]
+
+    cnn = CNN()
+    print(cnn)  # net architecture
+
+    optimizer = torch.optim.Adam(cnn.parameters(),
+                                 lr=LR)  # optimize all cnn parameters
+    loss_func = nn.CrossEntropyLoss()  # the target label is not one-hotted
+
+    # following function (plot_with_labels) is for visualization, can be ignored if not interested
+    from matplotlib import cm
+    try:
+        from sklearn.manifold import TSNE
+        HAS_SK = True
+    except:
+        HAS_SK = False
+        print('Please install sklearn for layer visualization')
+
+    plt.ion()
+    # training and testing
+    for epoch in range(EPOCH):
+        for step, (b_x, b_y) in enumerate(
+                train_loader
+        ):  # gives batch data, normalize x when iterate train_loader
+
+            output = cnn(b_x)[0]  # cnn output
+            loss = loss_func(output, b_y)  # cross entropy loss
+            optimizer.zero_grad()  # clear gradients for this training step
+            loss.backward()  # backpropagation, compute gradients
+            optimizer.step()  # apply gradients
+
+            if step % 50 == 0:
+                test_output, last_layer = cnn(test_x)
+                pred_y = torch.max(test_output, 1)[1].detach().numpy()
+                accuracy = float((pred_y == test_y.detach().numpy()
+                                  ).astype(int).sum()) / float(test_y.size(0))
+                print('Epoch: ', epoch,
+                      '| train loss: %.4f' % loss.detach().numpy(),
+                      '| test accuracy: %.2f' % accuracy)
+                if HAS_SK:
+                    # Visualization of trained flatten layer (T-SNE)
+                    tsne = TSNE(perplexity=30,
+                                n_components=2,
+                                init='pca',
+                                n_iter=5000)
+                    plot_only = 500
+                    low_dim_embs = tsne.fit_transform(
+                        last_layer.detach().numpy()[:plot_only, :])
+                    labels = test_y.numpy()[:plot_only]
+                    # plot_with_labels(low_dim_embs, labels)
+    plt.ioff()
+
+    # print 10 predictions from test data
+    test_output, _ = cnn(test_x[:10])
+    pred_y = torch.max(test_output, 1)[1].detach().numpy()
+    print(pred_y, 'prediction number')
+    print(test_y[:10].numpy(), 'real number')
