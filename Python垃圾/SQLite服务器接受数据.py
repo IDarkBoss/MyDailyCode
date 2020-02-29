f9c98e631666f827e71c5b380ece2fe825cad935
Lionel <472714747@qq.com>
2020-02-29 21:52:09 +0800

    1

--- Python垃圾/SQLite服务器接受数据.py
+++ Python垃圾/SQLite服务器接受数据.py
@@ -9,27 +9,29 @@ def hello():
     return "雷猴啊"
 
 
-@app.route('/register/<username,password>', methods=['POST'])
+@app.route('/register/<username>&<password>', methods=["GET", "POST"])
 def register(username, password):
-    conn = sqlite3.connect("/Handup_down.db")
+    conn = sqlite3.connect("/home/lionel/Handup_down.db")
     cur = conn.cursor()
     print("jajjajaa")
     # 是否已存在
     sql = "select * from USERS where USERNAME='" + username + "' and PASSWORD='" + password + "'"
     cur.execute(sql)
     data = cur.fetchone()
-    if len(data) > 0:
+    if data is not None:
         return "用户已存在"
     else:
-        sql = "insert into USERS [(USERNAME, PASSWORD, NAME, TIMES)]  VALUES ({username}, {password}, 'NAME', 0);"
+        sql = f"insert into USERS(USERNAME, PASSWORD, NAME, TIMES) VALUES ({username}, {password}, 'NAME', 0);"
         cur.execute(sql)
+        conn.commit()
         conn.close()
+        return "注册成功"
 
 
-@app.route('/login/<username,password>', methods=['GET'])
+@app.route('/login/<username>&<password>', methods=['GET'])
 def login(username, password):
     print("asdasds")
-    conn = sqlite3.connect("/Handup_down.db")
+    conn = sqlite3.connect("/home/lionel/Handup_down.db")
     cur = conn.cursor()
     sql = "select * from USERS where USERNAME='" + username + "' and PASSWORD='" + password + "'"
     cur.execute(sql)
@@ -43,4 +45,5 @@ def login(username, password):
 
 
 if __name__ == '__main__':
+    # app.debug = True
     app.run(host="0.0.0.0", port=8000)
