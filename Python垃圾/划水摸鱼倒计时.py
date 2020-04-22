c581e5160971f3ec321d3884a0b2973927bf289c
幻月丶沧歌 <26338127+IDarkBoss@users.noreply.github.com>
2020-04-22 09:04:53 +0800

    更改输出字体

--- Python垃圾/划水摸鱼倒计时.py
+++ Python垃圾/划水摸鱼倒计时.py
@@ -1,6 +1,5 @@
 # coding=utf-8
-import datetime, math
-import os
+import os, datetime, math
 
 
 def calculateTimeDiff():
@@ -21,16 +20,22 @@ def calculateTimeDiff():
         # 还没下班
         difference = offWork - now
         statu = "before"
+
+        # 获取相差的秒数
+        seconds = difference.seconds
+
+        # 间隔分钟数:
+        minutes = math.ceil(seconds / 60)
     else:
         # 早下班了
         difference = now - offWork
         statu = "after"
 
-    # 获取相差的秒数
-    seconds = difference.seconds
+        # 获取相差的秒数
+        seconds = difference.seconds
 
-    # 间隔分钟数:
-    minutes = math.ceil(seconds / 60)
+        # 间隔分钟数:
+        minutes = math.floor(seconds / 60)
 
     return statu, minutes
 
@@ -40,8 +45,8 @@ if __name__ == "__main__":
 
     statu, minutes = calculateTimeDiff()
     if statu == "before":
-        remainingTime = "还有" + str(minutes) + "分钟就要下班啦，老弟！"
+        remainingTime = "还有" + f"\033[1;33m{str(minutes)}\033[0m" + "分钟就要下班啦，小老弟！"
+        print(remainingTime)
     else:
-        beyond = "你都下班" + str(minutes) + "分钟啦，老弟！"
-
-    print(remainingTime)
+        beyondTime = "你都下班" + f"\033[33m{str(minutes)}\033[0m" + "分钟啦，小老弟！"
+        print(beyondTime)
