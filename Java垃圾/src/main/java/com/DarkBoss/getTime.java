8577adbc3a9eb3c767d3de3a792a606e9e73aa5c
IDarkBoss <abcce960611>
2020-06-19 00:37:25 +0800

    1

--- Java垃圾/src/main/java/com/DarkBoss/getTime.java
+++ Java垃圾/src/main/java/com/DarkBoss/getTime.java
@@ -11,18 +11,20 @@ import java.time.LocalTime;
 public class getTime {
     public static void main(String[] args) {
         LocalTime now = LocalTime.now();
-        LocalTime target = LocalTime.parse("18:00:00");
-        double duration = Duration.between(now, target).getSeconds();
+        LocalTime goToWork = LocalTime.parse("09:00:00");
+        LocalTime offWork = LocalTime.parse("18:00:00");
+        double duration_GoToWork = Duration.between(now, goToWork).getSeconds();
+        double duration_OffWork = Duration.between(now, offWork).getSeconds();
 
-        if (duration > 0) {
-            double minutes = Math.ceil(Math.abs(duration) / 60);
+        if (duration_OffWork > 0) {
+            double minutes = Math.ceil(Math.abs(duration_OffWork) / 60);
             DecimalFormat df1 = new DecimalFormat("0");
             String result = df1.format(minutes);
             System.out.println("你距离下班仅剩" + "\033[33m" + result + "\033[m" + "分钟啦，小老弟！");
-        } else if (duration == 0) {
+        } else if (duration_OffWork == 0) {
             System.out.println("下班啦！");
         } else {
-            double minutes = Math.floor(Math.abs(duration) / 60);
+            double minutes = Math.floor(Math.abs(duration_OffWork) / 60);
             DecimalFormat df1 = new DecimalFormat("0");
             String result = df1.format(minutes);
             System.out.println("你都已经下班" + "\033[33m" + result + "\033[m" + "分钟啦，小老弟！");
