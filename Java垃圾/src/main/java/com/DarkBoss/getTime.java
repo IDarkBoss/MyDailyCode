4ff216ed3969b4cbfb613b5f5529b3d5ee5d760b
幻月丶沧歌 <26338127+IDarkBoss@users.noreply.github.com>
2020-06-19 10:45:39 +0800

    添加上班计时

--- Java垃圾/src/main/java/com/DarkBoss/getTime.java
+++ Java垃圾/src/main/java/com/DarkBoss/getTime.java
@@ -8,29 +8,38 @@ import java.time.LocalTime;
  * @author Li Ang
  * @date 2020/4/27 下午 3:44
  */
-public class getTime {
+public class GetTime {
     public static void main(String[] args) {
+        GetTime getTime = new GetTime();
+        getTime.calTime();
+    }
+
+    private void calTime() {
         LocalTime now = LocalTime.now();
         LocalTime goToWork = LocalTime.parse("09:00:00");
         LocalTime offWork = LocalTime.parse("18:00:00");
-        double duration_GoToWork = Duration.between(now, goToWork).getSeconds();
-        double duration_OffWork = Duration.between(now, offWork).getSeconds();
+        double durationGoToWork = Duration.between(now, goToWork).getSeconds();
+        double durationOffWork = Duration.between(now, offWork).getSeconds();
 
-        if (duration_OffWork > 0) {
-            double minutes = Math.ceil(Math.abs(duration_OffWork) / 60);
+        if (durationGoToWork > 0) {
+            double minutes = Math.ceil(Math.abs(durationGoToWork) / 60);
+            DecimalFormat df1 = new DecimalFormat("0");
+            String result = df1.format(minutes);
+            System.out.println("你距离上班仅剩" + "\033[33m" + result + "\033[m" + "分钟啦，小老弟！");
+        } else if (durationGoToWork == 0) {
+            System.out.println("上班啦！！！");
+        } else if (durationGoToWork < 0 && durationOffWork > 0) {
+            double minutes = Math.ceil(Math.abs(durationOffWork) / 60);
             DecimalFormat df1 = new DecimalFormat("0");
             String result = df1.format(minutes);
             System.out.println("你距离下班仅剩" + "\033[33m" + result + "\033[m" + "分钟啦，小老弟！");
-        } else if (duration_OffWork == 0) {
-            System.out.println("下班啦！");
+        } else if (durationGoToWork < 0 && durationOffWork == 0) {
+            System.out.println("下班啦！！！");
         } else {
-            double minutes = Math.floor(Math.abs(duration_OffWork) / 60);
+            double minutes = Math.floor(Math.abs(durationOffWork) / 60);
             DecimalFormat df1 = new DecimalFormat("0");
             String result = df1.format(minutes);
             System.out.println("你都已经下班" + "\033[33m" + result + "\033[m" + "分钟啦，小老弟！");
         }
-
-
     }
-
 }
