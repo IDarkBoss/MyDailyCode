1ae98f87f9eed4e9e303d1c4b1d91d94aac47a2f
Lionel <472714747@qq.com>
2020-08-11 22:54:17 +0800

    引用hutool

--- Java/src/main/java/com/DarkBoss/DateCompare.java
+++ Java/src/main/java/com/DarkBoss/DateCompare.java
@@ -1,6 +1,8 @@
 package com.darkboss;
 
-import java.text.SimpleDateFormat;
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.date.DateUtil;
+
 import java.util.Calendar;
 import java.util.Date;
 
@@ -11,57 +13,42 @@ import java.util.Date;
  * @date 2020-06-19 15:54
  */
 public class DateCompare {
-    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
 
     public static void main(String[] args) {
         DateCompare dateCompare = new DateCompare();
-        dateCompare.calDuration("20200101", "20200606");
-    }
 
-    /**
-     * 日期 String 转 Date
-     *
-     * @param inputDate String
-     * @return Date
-     */
-    private Date toDate(String inputDate) {
-        try {
-            return DATE_FORMAT.parse(inputDate);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-
+        dateCompare.calDuration("20200111", "20200606");
     }
 
     /**
      * 计算间隔
      *
-     * @param fromDay 起始日
-     * @param toDay   目标日
+     * @param fromDay   起始日
+     * @param targetDay 目标日
      */
-    private void calDuration(String fromDay, String toDay) {
+    private void calDuration(String fromDay, String targetDay) {
         try {
-            String today = DATE_FORMAT.format(new Date());
-            if (fromDay.compareTo(toDay) > 0) {
+            // 方法一
+            Date fromDate = Convert.toDate(fromDay);
+            Date targetDate = Convert.toDate(targetDay);
+
+            // 方法二
+            // Date fromDate = new DateTime(fromDay, DatePattern.PURE_DATE_FORMAT);
+            // Date targetDate = new DateTime(targetDay, DatePattern.PURE_DATE_FORMAT);
+
+            Date today = new Date();
+            if (DateUtil.compare(fromDate, today) > 0) {
                 System.out.println("起始日期必须小于等于结束日期");
             }
-            if (toDay.compareTo(today) >= 0) {
+            if (DateUtil.compare(fromDate, targetDate) >= 0) {
                 System.out.println("结束日期必须小于当前日期");
             }
 
-            Date beforeDay = toDate(fromDay);
-            Date afterDay = toDate(toDay);
-
-            assert beforeDay != null;
-            assert afterDay != null;
-
-            Calendar calendar = Calendar.getInstance();
-            calendar.setTime(afterDay);
+            Calendar calendar = DateUtil.calendar(targetDate);
             calendar.add(Calendar.DAY_OF_MONTH, -7);
             Date before7days = calendar.getTime();
 
-            if (beforeDay.getTime() < before7days.getTime()) {
+            if (DateUtil.compare(fromDate, before7days) < 0) {
                 System.out.println("日期间隔不能大于7天");
             }
         } catch (Exception e) {
