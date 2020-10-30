b3d35695d3fce43bf00eb51386e544d5dc220fb1
Lionel <472714747@qq.com>
2020-10-30 11:00:08 +0800

    123

--- Java/src/main/java/com/DarkBoss/VariousTest.java
+++ Java/src/main/java/com/DarkBoss/VariousTest.java
@@ -1,5 +1,16 @@
 package com.darkboss;
 
+import com.alibaba.fastjson.JSON;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 /**
  * <h3>各种测试</h3>
  * <p>瞎写的</p>
@@ -7,6 +18,80 @@ package com.darkboss;
  * @author Lionel
  * @date 2020-08-16 18:22
  **/
+@Slf4j
 public class VariousTest {
+    public static void main(String[] args) {
+        // timeStampToDate("1603728000000");
+        // finalTest();
+        // optionalTest();
+        jsonObjectTest();
+    }
 
+    /**
+     * 时间戳转日期
+     *
+     * @param timeStamp 时间戳
+     */
+    public static void timeStampToDate(String timeStamp) {
+        Double time = Double.valueOf(timeStamp);
+        SimpleDateFormat sf = new SimpleDateFormat("yyyyMMdd");
+        String dateString = sf.format(time);
+        System.out.println(dateString);
+    }
+
+    /**
+     * finalTest
+     */
+    public static void finalTest() {
+        final Map<String, String> map = new HashMap<>(1);
+        map.put("1", "1");
+        map.put("2", "2");
+        System.out.println(map);
+    }
+
+    /**
+     * optional
+     */
+    public static void optionalTest() {
+        String aa = null;
+        log.info(Optional.ofNullable(aa).orElse("aa是null"));
+    }
+
+    /**
+     * JsonObject
+     */
+    public static void jsonObjectTest() {
+        ForwardData object = new ForwardData();
+        object.setDealDate(new Date());
+        object.setSpotDate(new Date());
+
+        ForwardBaseBO forwardBaseBO = new ForwardBaseBO();
+        forwardBaseBO.setData(object);
+        forwardBaseBO.setCusNumber(757);
+        HashMap<String, Object> taskMap = new HashMap<>(16);
+        taskMap.put("1231", forwardBaseBO);
+
+        log.info("远期taskMap:{}", JSON.toJSONString(taskMap));
+    }
+
+    @Data
+    public static class ForwardBaseBO implements Serializable {
+        private static final long serialVersionUID = 1L;
+        private Integer cusNumber;
+        private Date valuationDate;
+        private ForwardData data;
+    }
+
+    public static class ForwardData {
+        private Date dealDate;
+        private Date spotDate;
+
+        public void setDealDate(Date dealDate) {
+            this.dealDate = dealDate;
+        }
+
+        public void setSpotDate(Date spotDate) {
+            this.spotDate = spotDate;
+        }
+    }
 }
